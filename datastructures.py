ecfc03e0065dc08cf8ca4a878f82df899a485003
Hello, My name is Sammy Otia <131278278+drSammyOtia@users.noreply.github.com>
2023-09-07 08:45:50 -0700

    Add files via upload

---
+++ datastructures.py
@@ -0,0 +1,16 @@
+# Tuple
+stud_name = ("Erick", "Joshua", "Caleb", "Dennis", "Angie", "Collins")
+# stud_name[0]="Joshua"
+print(stud_name)
+print(stud_name[2])
+
+# List
+country_name = ["Kenya", "Rwanda", "Burundi", "Uganda"]
+numbers = [2, 5, 6, 1, 65, 74, 22, 3, 12, 0, 6]
+country_name[2] = "USA"
+print(country_name)
+print(sorted(numbers))
+
+# set
+Fruits = {"Mangoe", "Oranges", "Banana", "Watermelon"}
+print(Fruits)
---
+++ datastructures2.py
@@ -0,0 +1,18 @@
+# Tuple
+Employee_name = ["mark", "Tom", "Jane", "Willsmith", "Kennedy", "Brenda"]
+Employee_name[0] = "Joshua"
+print(Employee_name)
+print(Employee_name[2])
+
+# List
+entry_grade = ("A", "B", "C", "D", "F", "P")
+
+marks = (2, 5, 6, 1, 65, 74, 22, 3, 12, 0, 6)
+
+entry_grade == "D"
+print(entry_grade)
+print(sorted(marks))
+
+# set
+Sneakers = {"Jordan", "New Balance", "Converse", "Plimsoll"}
+print(Sneakers)
